5fb9663b33ac76c1cd775abe4095339b54975efe
JamesHong <james41015@gmail.com>
2019-08-23 16:28:14 +0800

    original

---
+++ RedSoDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedSoDemo
+//
+//  Created by Hong James on 2019/8/23.
+//  Copyright © 2019 Hong James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
